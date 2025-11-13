4eecff254b2ec1e8915170fb99eee55939c86ce7
David Pye <davidmpye@gmail.com>
2025-11-13 16:50:50 +0000

    Add timeout functionality to logger

--- fw/src/log_task.rs
+++ fw/src/log_task.rs
@@ -1,4 +1,8 @@
-use embassy_net::{Stack,dns::DnsSocket, tcp::client::{TcpClient, TcpClientState} };
+use embassy_net::{
+    dns::DnsSocket,
+    tcp::client::{TcpClient, TcpClientState},
+    Stack,
+};
 
 use embassy_rp::clocks::RoscRng;
 
@@ -7,24 +11,26 @@ use embassy_sync::channel::Channel;
 
 use defmt::*;
 
-use embassy_time::Timer;
+use embassy_time::{Timer, WithTimeout};
+use log::Log;
 use rand::RngCore;
 
-use reqwless::{request::RequestBuilder, response::StatusCode};
 use reqwless::client::{HttpClient, TlsConfig, TlsVerify};
 use reqwless::request::Method;
+use reqwless::{request::RequestBuilder, response::StatusCode};
 
 use crate::CONFIG;
 
 pub(crate) enum LogEvent {
-    ACTIVATED([u8;32]),
-    DEACTIVATED([u8;32]),
-    LOGINFAIL([u8;32]),
+    ACTIVATED([u8; 32]),
+    DEACTIVATED([u8; 32]),
+    LOGINFAIL([u8; 32]),
     ERROR,
 }
 
 //The queue can hold 32 events awaiting logging
-pub static LOG_EVENT_QUEUE: Channel::<ThreadModeRawMutex, LogEvent, 32> = Channel::<ThreadModeRawMutex, LogEvent, 32>::new();
+pub static LOG_EVENT_QUEUE: Channel<ThreadModeRawMutex, LogEvent, 32> =
+    Channel::<ThreadModeRawMutex, LogEvent, 32>::new();
 
 pub struct LogTaskRunner {
     stack: Stack<'static>,
@@ -37,92 +43,119 @@ impl LogTaskRunner {
 
     pub async fn run(self) -> ! {
         loop {
-            //If we have no wifi connection, try again in a bit.
+            //If we have no wifi connection, retry in 60 seconds
             while !self.stack.is_config_up() {
-                error!("Log task waiting - no wifi connection");
+                warn!("Log task waiting - no wifi connection");
                 //Wait 60 seconds and try again.
                 Timer::after_secs(60).await;
             }
 
-            //Block here waiting for an event to log
+            //Await an event from the queue
             let event = LOG_EVENT_QUEUE.receive().await;
-
-            //Convert hash to ascii string representation
-            let hash = match event {
-                LogEvent::ACTIVATED(hash)  | LogEvent::DEACTIVATED(hash) | LogEvent::LOGINFAIL(hash) => {
-                    //Convert hash to an ascii str representation
-                    hash
+            match self.log_event(&event)
+                .with_timeout(CONFIG.http_timeout)
+                .await {
+                    Ok(result) => {
+                        match result {
+                            Ok(_) => {
+                                info!("Log event recorded successfully")
+                            }
+                            Err(_) => {
+                                warn!("Log event failed, will be requeued");
+                                //Attempt to requeue
+                                if let Err(_e) = LOG_EVENT_QUEUE.try_send(event) {
+                                    error!("Unable to requeue - this event will be lost")
+                                }
+                            }
+                        }
+                    },
+                    Err(_timeout)=> {
+                        warn!("Log event timeout, will be requeued");
+                        //Attempt to requeue
+                        if let Err(_e) = LOG_EVENT_QUEUE.try_send(event) {
+                            error!("Unable to requeue - this event will be lost")
+                        }
+                    },
                 }
-                _ => {
-                    *b"N/A                             "
-                }
-            };
-            let hash = core::str::from_utf8(&hash).unwrap_or(" Non-ascii bytes in hash");
-
-            //Get printable name for event, as expected by the Makerspace logging API
-            let event_str = match event {
-                LogEvent::ACTIVATED(_) => {
-                    "Activated"
-                },
-                LogEvent::DEACTIVATED(_) => {
-                    "Deactivated"
-                },
-                LogEvent::LOGINFAIL(_) => {
-                    "LoginFail"
-                }
-                LogEvent::ERROR => {
-                    "ERROR"
-                }
-            };
-
-            //Build a fresh http client for each database update attempt
-            debug!("Reqwless HTTP client init");
-            let mut tls_read_buffer = [0; 8096];
-            let mut tls_write_buffer = [0; 8096];
-            let mut rng = RoscRng;
-            let seed = rng.next_u64();
-
-            let client_state = TcpClientState::<2, 1024, 1024>::new();
-            let tcp_client = TcpClient::new(self.stack, &client_state);
-            let dns_client = DnsSocket::new(self.stack);
-            let tls_config = TlsConfig::new(
-                seed,
-                &mut tls_read_buffer,
-                &mut tls_write_buffer,
-                TlsVerify::None,
-            );
-            let mut http_client= HttpClient::new_with_tls(&tcp_client, &dns_client, tls_config);
-
-            let mut url_buf = [0x00u8;128];
-            let url = format_no_std::show(&mut url_buf, format_args!("{}/{}/{}", CONFIG.url_endpoint, CONFIG.device_name, CONFIG.log_prefix)).expect("Unable to build DB update URL");
-            debug!("Connecting to {}", &url);
-
-            let mut json_buf = [0x00;256];
-            let json = format_no_std::show(&mut json_buf, format_args!("{{ \"type\": \"{}\", \"hash\": \"{}\"}}", event_str, hash)).expect("Unable to build JSON log event");
-
-            debug!("Json string: {}", json);
-            
-            let mut resp_buf = [0x00;128];
-
-            if let Ok(req)  = http_client.request(Method::POST, &url).await {
-                if let Ok(e) = req.content_type(reqwless::headers::ContentType::ApplicationJson)
-                    .body(json.as_bytes()).send(&mut resp_buf).await {
-                    debug!("Log event POSTed successfully");
-                }
-                else {
-                    error!("Unable to log event - will attempt to requeue");
-                    //NB this will cause the event to go to the back of the queue, so its' order will be wrong
-                    match LOG_EVENT_QUEUE.try_send(event) {
-                        Ok(_) => {
-                            debug!("Log event requeued");
-                        },
-                        Err(_) => {
-                            error!("Unable to requeue failed event - it will be lost");
-                        },
-                    }
-                }
-            };
-
-       }
+        }
     }
+
+    async fn log_event(&self, event: &LogEvent) -> Result<(), ()> {
+        //Convert hash to ascii string representation
+        let hash = match event {
+            LogEvent::ACTIVATED(hash) | LogEvent::DEACTIVATED(hash) | LogEvent::LOGINFAIL(hash) => {
+                //Convert hash to an ascii str representation
+                hash
+            }
+            _ => b"N/A                             ",  //32 bytes long too...
+        };
+        let hash = core::str::from_utf8(hash).unwrap_or(" Non-ascii bytes in hash");
+
+        //Get printable name for event, as expected by the Makerspace logging API
+        let event_str = match event {
+            LogEvent::ACTIVATED(_) => "Activated",
+            LogEvent::DEACTIVATED(_) => "Deactivated",
+            LogEvent::LOGINFAIL(_) => "LoginFail",
+            LogEvent::ERROR => "ERROR",
+        };
+
+        //Build a fresh http client for each database update attempt
+        info!("Log task making connection to log endpoint");
+        let mut tls_read_buffer = [0; 8096];
+        let mut tls_write_buffer = [0; 8096];
+        let mut rng = RoscRng;
+        let seed = rng.next_u64();
+
+        let client_state = TcpClientState::<2, 1024, 1024>::new();
+        let tcp_client = TcpClient::new(self.stack, &client_state);
+        let dns_client = DnsSocket::new(self.stack);
+        let tls_config = TlsConfig::new(
+            seed,
+            &mut tls_read_buffer,
+            &mut tls_write_buffer,
+            TlsVerify::None,
+        );
+        let mut http_client = HttpClient::new_with_tls(&tcp_client, &dns_client, tls_config);
+
+        let mut url_buf = [0x00u8; 128];
+        let url = format_no_std::show(
+            &mut url_buf,
+            format_args!(
+                "{}/{}/{}",
+                CONFIG.url_endpoint, CONFIG.device_name, CONFIG.log_prefix
+            ),
+        )
+        .expect("Unable to build DB update URL");
+        debug!("Connecting to {}", &url);
+
+        let mut json_buf = [0x00; 256];
+        let json = format_no_std::show(
+            &mut json_buf,
+            format_args!("{{ \"type\": \"{}\", \"hash\": \"{}\"}}", event_str, hash),
+        )
+        .expect("Unable to build JSON log event");
+
+        debug!("Json string: {}", json);
+
+        let mut rx_buf = [0x00; 512];
+
+        let x = if let Ok(req) = http_client.request(Method::POST, &url).await {
+            if let Ok(_e) = req
+                .content_type(reqwless::headers::ContentType::ApplicationJson)
+                .body(json.as_bytes())
+                .send(&mut rx_buf)
+                .await
+            {
+                Ok(())
+            } else {
+                error!("HTTP response error");
+                Err(())
+            }
+        } else {
+            error!("Client request error");
+            Err(())
+        };
+        x
+    }
+
 }
